473b2f79833d55fe99696ebef715d6485bf6be4f
Noah Platton <noahplatton@gmail.com>
2022-02-16 17:21:52 +0000

    Use dayjs to update dates of created blogs

--- api/controllers/blogs.js
+++ api/controllers/blogs.js
@@ -1,5 +1,9 @@
 const Blog = require("../models/Blog");
 
+const dayjs = require("dayjs");
+var relativeTime = require("dayjs/plugin/relativeTime");
+dayjs.extend(relativeTime);
+
 async function index(req, res) {
   try {
     const blogs = await Blog.all;
@@ -20,7 +24,11 @@ async function show(req, res) {
 
 async function create(req, res) {
   try {
-    const blog = await Blog.create(req.body);
+    const data = req.body;
+    data.year = dayjs().format("DD/MM/YYYY").toString().split("/")[2];
+    data.month = dayjs().format("DD/MM/YYYY").toString().split("/")[1];
+    data.day = dayjs().format("DD/MM/YYYY").toString().split("/")[0];
+    const blog = await Blog.create(data);
     res.status(201).json(blog);
   } catch (err) {
     res.status(422).json({ err });
